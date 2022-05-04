2e2ab24aeaa433d41515f1c122d745892efc491b
Henrique Barros <henriquebarrosx@gmail.com>
2022-05-04 20:47:30 -0300

    Make spell creation page responsive

--- src/pages/Spells/Creation/index.tsx
+++ src/pages/Spells/Creation/index.tsx
@@ -1,11 +1,12 @@
 import { ChangeEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { SpellType } from "./LeftSide";
-import { FormView } from "./RightSide";
-import { Header } from "@components/Header";
+import { LeftSide } from "./LeftSide";
+import { RightSide } from "./RightSide";
+import { HeaderSide } from "@components/Header";
+import { SubmitButton } from "@components/SubmitButton";
 import { createSpell } from "@services/network/saveSpell";
-import { SpellContextProvider, useSpell } from "@store/SpellCreation";
+import { SpellContextProvider, useSpell } from "@store/Spell";
 
 function SpellCreationComponent(): JSX.Element {
   const navigate = useNavigate();
@@ -14,10 +15,12 @@ function SpellCreationComponent(): JSX.Element {
   const [spellName, setSpellName] = useState<string>('');
   const [isSubmitting, shouldDisplaySubmissionFeedback] = useState(false);
 
-  const submitButtonLabel = isSubmitting ? "Cadastrando..." : "Cadastrar Magia";
-
   function onSpellNameChange(event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void {
     setSpellName(event.target.value)
+  }
+
+  function getSubmitButtonLabel(): string {
+    return isSubmitting ? "Cadastrando..." : "Cadastrar Magia";
   }
 
   async function createNewSpell(): Promise<void> {
@@ -34,25 +37,26 @@ function SpellCreationComponent(): JSX.Element {
   }
 
   return (
-    <div className="flex flex-col relative h-screen">
-      <Header />
+    <div className="flex flex-col relative">
+      <HeaderSide />
 
-      <div className="flex justify-center items-center h-full">
-        <div className="flex flex-col bg-[#242424] box-shadow-lg w-[871px] h-[491px] rounded-lg p-14">
-          <div className="flex">
-            <SpellType />
+      <div className="flex justify-center items-center h-full mt-24 mb-24 md:mt-[10%] lg:mt-[150px] md:mb-0">
+        <div className="flex flex-col md:px-14 md:py-24 px-8 py-8 bg-[#242424] box-shadow-lg md:max-w-[871px] w-[90%] sm:max-h-[unset] rounded-lg">
+          <div className="flex md:flex-row flex-col-reverse">
+            <LeftSide />
 
-            <FormView
+            <RightSide
               spellName={spellName}
               onSpellNameChange={onSpellNameChange}
             />
           </div>
 
-          <div className="self-center mt-16">
-            <button onClick={createNewSpell} disabled={isSubmitting} title="Cadastrar magia" className="w-[300px] uppercase bg-[#6C63FF] h-12 text-sm text-white font-bold rounded-lg hover:bg-[#403A9E] duration-200 ease-linear disabled:opacity-30 disabled:bg-[#6C63FF] disabled:cursor-not-allowed">
-              {submitButtonLabel}
-            </button>
-          </div>
+          <SubmitButton
+            disabled={isSubmitting}
+            onSubmit={createNewSpell}
+            label={getSubmitButtonLabel()}
+            tooltipMessage="Cadastrar magia"
+          />
         </div>
       </div>
     </div>
