0f0bcd7b68376e14265d3afc86190560c0d49499
Henrique Barros <henriquebarrosx@gmail.com>
2022-05-05 01:06:18 -0300

    Add validation message to sign in page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,20 +3,28 @@ import { useNavigate } from "react-router-dom";
 
 import { useSession } from "@store/Session";
 import { HeaderSide } from "@components/Header";
+import { validateWithName } from "@utils/witchNameValidations";
 
 export function SignIn(): JSX.Element {
   const navigate = useNavigate();
-  const { setWitchName} = useSession();
+  const { setWitchName } = useSession();
 
   const [inputValue, setInputValue] = useState<string>('');
+  const [validationMessage, setValidationMessage] = useState("");
 
   function onWitchNameChange(event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void {
     setInputValue(event.target.value)
   }
 
   async function signIn(): Promise<void> {
+    const validation = validateWithName(inputValue);
+
+    if (validation) {
+      return setValidationMessage(validation);
+    }
+
     setWitchName(inputValue.toUpperCase());
-    navigate('/spells');
+    return navigate('/spells');
   }
 
   return (
@@ -24,7 +32,7 @@ export function SignIn(): JSX.Element {
       <HeaderSide />
 
       <main className="flex items-center justify-center h-screen">
-        <form className="px-8 md:px-16 py-10 w-[90%] bg-[#242424] shadow-lg shadow-[#6C63FF] rounded-lg max-w-[460px] self-center flex-col border-2 border-solid border-[#6C63FF]">
+        <div className="px-8 md:px-16 py-10 w-[90%] bg-[#242424] shadow-lg shadow-[#6C63FF] rounded-lg max-w-[460px] self-center flex-col border-2 border-solid border-[#6C63FF]">
           <h1 className="text-white font-bold text-2xl text-center mb-7 capitalize">
             Realizar login
           </h1>
@@ -37,6 +45,12 @@ export function SignIn(): JSX.Element {
             className="w-full rounded-md h-12 bg-[#202024] px-4 caret-white text-white placeholder:text-[#929293] placeholder:uppercase text-sm capitalize focus:outline-none focus:border-[#6C63FF] focus:border-solid focus:border-[1px]"
           />
 
+          {!!validationMessage && (
+            <div className="ml-2 mt-2">
+              <span className="text-red-400">{validationMessage}</span>
+            </div>
+          )}
+
           <div className="mt-6 mb-7">
             <span className="text-[#929293] font-bold text-xs uppercase">
               “Seja aprendiz, experiente ou um grão mestre, o nome de um feiticeiro será aquele que ecoará aos quatro quantos da terra por seus grandes feitos!”
@@ -46,7 +60,7 @@ export function SignIn(): JSX.Element {
           <button onClick={signIn} className="uppercase bg-[#6C63FF] h-12 text-sm text-white font-bold rounded-lg w-full hover:bg-[#403A9E] duration-200 ease-linear">
             Entrar
           </button>
-        </form>
+        </div>
       </main>
     </div>
   )
