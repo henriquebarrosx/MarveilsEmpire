6a501cf19c9efafdca5bf14be66e92d8b4a82ef9
Henrique Barros <henriquebarrosx@gmail.com>
2022-05-05 04:54:32 -0300

    adding deploy config

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 import { SignIn } from '@pages/SignIn';
+import { NotFoundPage } from '@pages/404';
 import { SpellList } from '@pages/Spells/List';
 import { SpellDetails } from '@pages/Spells/Details';
 import { SpellCreation } from '@pages/Spells/Creation';
@@ -9,6 +10,7 @@ export function Router(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<NotFoundPage />} />
         <Route path="/signIn" element={<SignIn />} />
         <Route path="spells" element={<SpellList />} />
         <Route path="spells/new" element={<SpellCreation />} />
