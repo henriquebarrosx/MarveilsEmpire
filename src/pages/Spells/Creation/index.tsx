74bfc8057f2c84aa247d3dcb6b1b8e55f151576a
Henrique Barros <henriquebarrosx@gmail.com>
2022-05-04 23:05:32 -0300

    Add loading feedback animation after request to create new spell

--- src/pages/Spells/Creation/index.tsx
+++ src/pages/Spells/Creation/index.tsx
@@ -7,6 +7,7 @@ import { HeaderSide } from "@components/Header";
 import { SubmitButton } from "@components/SubmitButton";
 import { createSpell } from "@services/network/spell/create";
 import { SpellContextProvider, useSpell } from "@store/Spell";
+import { LoadingIndicator } from "@components/LoadingIndicator";
 
 function SpellCreationComponent(): JSX.Element {
   const navigate = useNavigate();
@@ -42,6 +43,8 @@ function SpellCreationComponent(): JSX.Element {
 
       <div className="flex justify-center items-center h-full mt-24 mb-24 md:mt-[10%] lg:mt-[150px] md:mb-0">
         <div className="flex flex-col md:px-14 md:py-24 px-8 py-8 bg-[#242424] box-shadow-lg md:max-w-[871px] w-[90%] sm:max-h-[unset] rounded-lg">
+          <LoadingIndicator isVisible={isSubmitting} />
+
           <div className="flex md:flex-row flex-col-reverse">
             <LeftSide />
 
