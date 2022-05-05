e95e65a610b3cc40d67fefeb4f1bfe378de557e7
Henrique Barros <henriquebarrosx@gmail.com>
2022-05-05 00:36:53 -0300

    Resize small submit button

--- src/components/SubmitButton/index.tsx
+++ src/components/SubmitButton/index.tsx
@@ -16,14 +16,14 @@ function SubmitButtonComponent(props: Props): JSX.Element {
   }
 
   return (
-    <button onClick={onSubmit} disabled={disabled} title={tooltipMessage} style={{ width: getButtonWidth() }} className="uppercase bg-[#6C63FF] h-12 text-sm text-white font-bold rounded-lg hover:bg-[#403A9E] duration-200 ease-linear disabled:opacity-30 disabled:bg-[#6C63FF] disabled:cursor-not-allowed">
+    <button onClick={onSubmit} disabled={disabled} title={tooltipMessage} style={{ width: getButtonWidth() }} className="uppercase bg-[#6C63FF] h-12 text-sm text-white font-bold rounded-md hover:bg-[#403A9E] duration-200 ease-linear disabled:opacity-30 disabled:bg-[#6C63FF] disabled:cursor-not-allowed">
       {label}
     </button>
   )
 }
 
 const buttonWidth = {
-  small: 200,
+  small: 120,
   default: 300,
 }
 
