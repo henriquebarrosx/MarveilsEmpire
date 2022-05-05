5ce8b844ee31b84f73b24ff04ad96105461cf306
Henrique Barros <henriquebarrosx@gmail.com>
2022-05-05 00:36:37 -0300

    Fix popover UI in small resolution

--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -12,7 +12,7 @@ export interface IAction {
 export function Popover({ isVisible, actions }: Props): JSX.Element | null {
   if (isVisible) {
     return (
-      <div className="flex flex-col shadow-lg bg-[#424242] rounded-lg absolute -right-20 -top-16 z-30">
+      <div className="flex flex-col shadow-lg bg-[#424242] rounded-lg absolute md:-right-20 right-4 -top-16 z-30">
         {actions.map(({ label, onClick, toolTipMessage: toolTip }) => (
           <button key={label} onClick={onClick} className="capitalize text-white hover:bg-[#373737] px-6 h-10 duration-200 rounded-lg" title={toolTip}>
             {label}
